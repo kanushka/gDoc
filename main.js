ce1d0b05c6676061eb8b32954eeea2cc92339e7f
Kanushka Gayan <kanushkanet@gmail.com>
2018-10-02 22:02:38 +0530

    initial files

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const {
+    app,
+    BrowserWindow
+} = electron;
+
+let mainWindow;
+
+app.on("ready", function () {
+    // create main window
+    mainWindow = new BrowserWindow({});
+
+    // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
